dffa9b976abd89e5a95d8ce1e08c4646ed0e1623
Michée Allidjinou <allidjinoumichee@outlook.fr>
2023-07-18 09:44:03 +0200

    Add darkmode toggle

---
+++ src/components/DarkmodeToggle.svelte
@@ -0,0 +1,20 @@
+<script>
+	let darkMode = true;
+
+	function toggle() {
+		darkMode = !darkMode;
+
+		darkMode
+			? document.documentElement.classList.add('dark')
+			: document.documentElement.classList.remove('dark');
+
+		console.log(document.documentElement.classList);
+	}
+</script>
+
+<button on:click={toggle}>
+	{darkMode ? 'Go Light' : 'Go Dark'}
+</button>
+
+<style>
+</style>
