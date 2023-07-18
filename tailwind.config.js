ba52fec9f7a35f2c8fafe763df910058314eab7a
Michée Allidjinou <allidjinoumichee@outlook.fr>
2023-07-18 09:58:22 +0200

    Add JetBrains font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    fontFamily: {
+      'mono': ['JetBrains Mono', 'monospace']
+    },
     extend: {},
   },
   plugins: [],
