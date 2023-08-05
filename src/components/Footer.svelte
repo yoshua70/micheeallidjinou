2266fe46e75d5087e3521b231e3064c78a71faf8
Michée Allidjinou <allidjinoumichee@outlook.fr>
2023-08-05 09:46:44 +0200

    Update footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,5 +1,7 @@
-<footer class="flex items-center justify-center p-4">
-	<p class="font-light font-mono text-sm">
+<footer
+	class="flex flex-col sm:flex-row justify-center items-center sm:items- gap-4 p-6 font-mono bg-slate-100 dark:bg-slate-950"
+>
+	<p class="font-light font-mono text-sm text-center">
 		2023 Michée Allidjinou. Find the source <a
 			href="https://github.com/yoshua70/micheeallidjinou"
 			class="underline">here</a
