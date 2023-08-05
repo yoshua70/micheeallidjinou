c9c7d94add65e1e6c1172f3ac886ddb3924c9511
Michée Allidjinou <allidjinoumichee@outlook.fr>
2023-08-05 09:46:57 +0200

    Complete about section

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,20 @@
 <div class="flex flex-col justify-center items-start w-full lg:w-1/2 gap-4">
-	<h1 class="text-right">About</h1>
+	<h1 class="font-mono font-bold text-5xl">About</h1>
+	<p>
+		Computer science is too big of a subject, so interesting that it would be sad to limit yourself
+		to one field. That's why I try to learn and touch to as many fields, subject, and technologies
+		in computer science (did I hear Jack of all trade in the back?). And you name it:
+	</p>
+	<ul class="list-disc w-full px-8">
+		<li>Web development</li>
+		<li>System and low-level programming</li>
+		<li>Cybersecurity and pentesting</li>
+		<li>AI and machine learning</li>
+	</ul>
+	<p>
+		That's why I prefer to refer to myself simply as a Software Engineer. Of course, I don't not
+		claim to master every or any of those fields, but I definitively know what where are, and
+		practice them when I can.
+	</p>
+	<p>So yeah, that's all.</p>
 </div>
